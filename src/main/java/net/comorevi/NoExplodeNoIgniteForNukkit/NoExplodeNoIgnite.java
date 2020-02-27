f94617db83ffa371c8d76bb86b8d1b4fc37ac351
こもれび / popkechupki <hobbypopkechupki@gmail.com>
2020-02-27 22:47:52 +0900

    Update to 1.3.0.
    バケツでもマグマを流せないように

--- src/main/java/net/comorevi/NoExplodeNoIgniteForNukkit/NoExplodeNoIgnite.java
+++ src/main/java/net/comorevi/NoExplodeNoIgniteForNukkit/NoExplodeNoIgnite.java
@@ -2,7 +2,9 @@ package net.comorevi.NoExplodeNoIgniteForNukkit;
 
 import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockLava;
+import cn.nukkit.event.block.BlockPlaceEvent;
 import cn.nukkit.event.block.BlockUpdateEvent;
+import cn.nukkit.event.player.PlayerBucketEmptyEvent;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.EventHandler;
@@ -37,6 +39,26 @@ public class NoExplodeNoIgnite extends PluginBase implements Listener {
 				event.setCancelled();
 				getServer().broadcastMessage(NoExplodeNoIgnite.prefix + "ブロックへの着火をキャンセルしました。\n - " + TextFormat.YELLOW + event.getEntity().getName() + TextFormat.WHITE + "が火打石を使用しました。");
 				break;
+		}
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event) {
+		switch (event.getBlock().getId()) {
+			case Block.LAVA:
+			case Block.WATER:
+				if (!event.getPlayer().isOp()) {
+					event.setCancelled();
+					getServer().broadcastMessage(NoExplodeNoIgnite.prefix + "マグマ・水ブロックの設置をキャンセルしました。\n - " + TextFormat.YELLOW + event.getPlayer().getName() + TextFormat.WHITE + "が制限されたアイテムを使用しました。");
+				}
+		}
+	}
+
+	@EventHandler
+	public void onBucketEmpty(PlayerBucketEmptyEvent event) {
+		if (event.getBucket().getName().equals("Lava Bucket") && !event.getPlayer().isOp()) {
+			event.setCancelled();
+			getServer().broadcastMessage(NoExplodeNoIgnite.prefix + "マグマは流せません。\n - " + TextFormat.YELLOW + event.getPlayer().getName() + TextFormat.WHITE + "がバケツからマグマを流そうとしました。");
 		}
 	}
 
